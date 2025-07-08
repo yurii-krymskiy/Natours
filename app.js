75ad243e56500a651019b8ee4b20b2c65f99e6e6
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-08 15:21:02 +0200

    parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -28,6 +29,18 @@ app.use(express.json({ limit: '10kb' }));
 
 app.use(mongoSanitize());
 app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 app.use(express.static(`${__dirname}/public`));
 
