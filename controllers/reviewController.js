0e901ad32dc3335640ae3ad3885241ec03c1c6b2
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-09 21:05:12 +0200

    add review

---
+++ controllers/reviewController.js
@@ -0,0 +1,33 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res) => {
+  const reviews = await Review.find().populate();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res) => {
+  if (!req.body.tour) {
+    req.body.tour = req.params.tourId;
+  }
+
+  if (!req.body.user) {
+    req.body.user = req.user.id;
+  }
+
+  const newReview = await Review.create(req.body);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      newReview,
+    },
+  });
+});
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -116,6 +115,12 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
@@ -128,13 +133,6 @@ tourSchema.pre(/^find/, function (next) {
   });
   next();
 });
-
-// tourSchema.pre('save', async function (next) {
-//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-
-//   this.guides = await Promise.all(guidesPromises);
-//   next();
-// });
 
 tourSchema.pre('find', function (next) {
   this.find({ secretTour: { $ne: true } });
